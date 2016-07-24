5a4a11c52446438da5ff3ce9c3d896849b527e1b
flonly2008 <liuzhenhua_sf@baidu.com>
2016-07-24 16:51:56 +0800

    Process User Input

--- QuickStart/QuickStart/ViewController.swift
+++ QuickStart/QuickStart/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -24,12 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
     
     // MARK: Actions
     
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
+    
+    
     
 }
 
