dbeaaab09ac2534f6b4246aa3b7368b74f821574
flonly2008 <liuzhenhua_sf@baidu.com>
2016-07-24 18:12:22 +0800

    Implement a Custom Control Create a Custom View

---
+++ QuickStart/QuickStart/RatingControl.swift
@@ -0,0 +1,25 @@
+//
+//  RatingControl.swift
+//  QuickStart
+//
+//  Created by Flonly on 7/24/16.
+//  Copyright © 2016 FlonlyIos. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
